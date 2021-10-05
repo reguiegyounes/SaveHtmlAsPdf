69694fbde1a3fea4a9fb590017b13d8cd334bc21
younes <reguiegyounes@gmail.com>
2021-10-05 15:31:09 +0100

    create folder /Uploads if not exist

--- upload.php
+++ upload.php
@@ -3,6 +3,10 @@
         $pdf_content = $_POST['data'];
         //Decode pdf content
         $pdf_decoded = base64_decode ($pdf_content);
+        // create folder 'uploads' if not exist 
+        if (!file_exists(__DIR__.'/uploads')) {
+            mkdir(__DIR__.'/uploads', 0777, true);
+        }
         //Write data back to pdf file
         $pdf = fopen (__DIR__.'/uploads/test.pdf','w');
         fwrite ($pdf,$pdf_decoded);
